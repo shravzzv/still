4f97dcfe03f5a8b892f253a78d42e6d84338ce66
Sai Shravan <saishravan384@gmail.com>
2026-02-11 19:41:20 +0530

    test: setup e2e test tooling

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from 'react-native'
 export default function Index() {
   return (
     <View className='flex-1 items-center justify-center bg-surface'>
-      <Text className='text-primary text-2xl font-bold underline'>Still</Text>
+      <Text className='text-primary text-2xl font-bold underline'>
+        Still like water
+      </Text>
     </View>
   )
 }
