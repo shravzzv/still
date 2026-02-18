9bc2332bfb25cb3cefb83edcd5e14d445abe0556
Sai Shravan Vadla <94429984+shravzzv@users.noreply.github.com>
2026-02-18 15:34:47 +0530

    feat(ui): introduce static task list surface (#3)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,48 @@
 import EmptyTasksState from '@/components/empty-tasks-state'
+import TaskRow from '@/components/task-row'
 import { Screen } from '@/components/ui/screen'
 import type { Task } from '@/types/task'
 import { useState } from 'react'
+import { FlatList } from 'react-native'
+
+const intialState: Task[] = [
+  ...Array.from({ length: 0 }, (_, i) => ({
+    id: `temp-${i}`,
+    title: `Task ${i + 1}: Check if scrolling is smooth`,
+    completed: i % 3 === 0,
+    createdAt: '',
+  })),
+  { id: '1', title: 'Breathe', completed: false, createdAt: '' },
+  { id: '2', title: 'Drink water', completed: true, createdAt: '' },
+  { id: '3', title: 'Step outside', completed: false, createdAt: '' },
+]
 
 export default function Page() {
-  const [tasks, setTasks] = useState<Task[]>([])
-  const isEmpty = tasks.length === 0
+  const [tasks, setTasks] = useState<Task[]>(intialState)
 
-  return <Screen>{isEmpty && <EmptyTasksState />}</Screen>
+  const completeTask = (id: string) => {
+    setTasks((prev) =>
+      prev.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task,
+      ),
+    )
+  }
+
+  return (
+    <Screen>
+      <FlatList
+        className="w-full rounded-xl"
+        contentContainerClassName="w-full mx-auto max-w-2xl flex flex-col gap-2 rounded-xl"
+        ListEmptyComponent={<EmptyTasksState />}
+        data={tasks}
+        keyExtractor={(item) => item.id}
+        renderItem={(task) => (
+          <TaskRow
+            task={task.item}
+            complete={() => completeTask(task.item.id)}
+          />
+        )}
+      />
+    </Screen>
+  )
 }
