504dff16efa8321e340d854bcea532d365806204
Sai Shravan <saishravan384@gmail.com>
2026-02-25 21:13:51 +0530

    feat: update not-found page

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,15 +1,29 @@
+import { Screen } from '@/components/ui/screen'
 import { Text } from '@/components/ui/text'
 import { Link } from 'expo-router'
 import { View } from 'react-native'
 
 export default function Page() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl">There&apos;s nothing here</Text>
+    <Screen className="items-center justify-center">
+      <View className="max-w-sm items-center gap-4">
+        <Text
+          className="text-center font-medium text-2xl"
+          accessibilityRole="header"
+        >
+          Page not found
+        </Text>
 
-      <Link href="/" className="underline underline-offset-2" asChild>
-        <Text>Go to home</Text>
-      </Link>
-    </View>
+        <Text className="text-center text-sm text-muted dark:text-muted-dark">
+          The link might be broken or the page may have moved.
+        </Text>
+
+        <Link href="/" asChild>
+          <Text className="text-primary underline underline-offset-2 dark:text-primary-dark">
+            Back to home
+          </Text>
+        </Link>
+      </View>
+    </Screen>
   )
 }
