1187b391dfc5a8b449d1ef6fbdc9fefbfa2c6e03
Sai Shravan <saishravan384@gmail.com>
2026-06-02 20:23:47 +0530

    chore: format repo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function Page() {
-  return <div>Hello world</div>
+  return (
+    <div>
+      <Button>Add</Button>
+    </div>
+  )
 }
