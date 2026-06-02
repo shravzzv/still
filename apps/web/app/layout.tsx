6f2d1bd10cdd5228f47c27fe2386ed73894b0d0b
Sai Shravan <saishravan384@gmail.com>
2026-06-02 20:55:58 +0530

    fix(web): supress hydration warning on html tag

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,20 +2,10 @@ import { cn } from '@/lib/utils'
 import { ThemeProvider } from '@/providers/theme-provider'
 import { Analytics } from '@vercel/analytics/next'
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono, Inter } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Still',
@@ -31,16 +21,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn(
-        'h-full',
-        'antialiased',
-        geistSans.variable,
-        geistMono.variable,
-        'font-sans',
-        inter.variable,
-      )}
+      className={cn('antialiased', 'font-sans', inter.variable)}
+      suppressHydrationWarning
     >
-      <body className="flex min-h-full flex-col">
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
