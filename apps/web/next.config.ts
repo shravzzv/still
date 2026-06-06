449c8a26e77f780eb0b6b37228d0eff484fda2dc
Sai Shravan <saishravan384@gmail.com>
2026-06-06 15:12:30 +0530

    chore: update next config to generate an out folder on build

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.0.103'],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
