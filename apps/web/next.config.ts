df86b038c1b1aa9a36f7f89d451be6de8e27b25a
Sai Shravan <saishravan384@gmail.com>
2026-06-15 09:08:58 +0530

    chore: configure serwist

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,3 +1,4 @@
+import { withSerwist } from '@serwist/turbopack'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
@@ -5,4 +6,4 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.0.103'],
 }
 
-export default nextConfig
+export default withSerwist(nextConfig)
